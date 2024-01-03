6b6b2ba4e684ec5550c04cbe640d21bd4592a040
Akhil <88569103+RapidCow1@users.noreply.github.com>
2024-01-03 21:51:27 +0800

    Allow for hover drawing/clearing

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     updateGrid();
 });
 
+function changeColour() {
+    event.target.style.backgroundColor = 'blue';
+}
+
+function clearGrid() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = 'white';
+    });
+}
+
 function updateGrid() {
     const gridSize = parseInt(document.getElementById('gridSize').value) || 5;
     const container = document.getElementById('gridContainer');
@@ -11,6 +22,8 @@ function updateGrid() {
     for (let i = 0; i < gridSize * gridSize; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.addEventListener('mouseover', changeColour);
+        //square.onclick = changeColour;
         container.appendChild(square);
     }
 }
